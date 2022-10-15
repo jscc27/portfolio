da80d5cb5b8710eb8d37def29c6eb39205eaa6d0
Juliano <julianosouzach@gmail.com>
2022-10-14 23:05:43 -0400

    change buttons in home page 3

--- public/Scripts/home.js
+++ public/Scripts/home.js
@@ -1,5 +1,21 @@
 
-document.getElementById("aboutBtn").addEventListener("click", getHome);
+document.getElementById("aboutBtn").addEventListener("click", getAbout);
+
+function getAbout(){
+    res.render('about', { title: 'Home' });
+}
+
+function getHome(){
+    res.render('index', { title: 'Home' });
+}
+
+function getHome(){
+    res.render('index', { title: 'Home' });
+}
+
+function getHome(){
+    res.render('index', { title: 'Home' });
+}
 
 function getHome(){
     res.render('index', { title: 'Home' });
