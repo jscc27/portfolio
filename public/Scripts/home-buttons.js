c073c52c9d2cb2ac3813ce567c98be2d5dd3d94e
Juliano <julianosouzach@gmail.com>
2022-10-16 22:56:55 -0400

    add comments

--- public/Scripts/home-buttons.js
+++ public/Scripts/home-buttons.js
@@ -4,18 +4,22 @@ document.getElementById("projectsBtn").addEventListener("click", getProjects);
 document.getElementById("servicesBtn").addEventListener("click", getServices);
 document.getElementById("contactBtn").addEventListener("click", getContact);
 
+//Function to get the about page when the button is clicked
 function getAbout(){
     location.href = '/about';
 }
 
+//Function to get the projects page when the button is clicked
 function getProjects(){
     location.href = '/projects';
 }
 
+//Function to get the services page when the button is clicked
 function getServices(){
     location.href = '/services';
 }
 
+//Function to get the contact page when the button is clicked
 function getContact(){
     location.href = '/contact';
 }
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,8 @@
+
+// Name: Juliano Souza Chaves Costa
+// StudentID: 301222713
+// Date: 2022-10-16
+
 var express = require('express');
 var router = express.Router();
 
